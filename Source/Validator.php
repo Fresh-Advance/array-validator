bf51a4ffe4c87cde15836d265096cd0bae1f970e
Anton Fedurtsya <anton@fedurtsya.com>
2017-11-15 00:55:47 +0200

    Validation basic implementation

--- Source/Validator.php
+++ Source/Validator.php
@@ -2,12 +2,62 @@
 
 namespace Sieg\ArrayValidator;
 
+use Sieg\ArrayValidator\Exception\RuleFailed;
+use Sieg\ArrayValidator\Rule\AbstractRule;
+
 class Validator
 {
+    /** @var array $rules */
+    protected $rules;
+
+    /** @var array $errors */
+    protected $errors = [];
+
     /**
      * Validator constructor.
+     *
+     * @var array
      */
-    public function __construct()
+    public function __construct($validationRules = [])
     {
+        $this->rules = $validationRules;
+    }
+
+    /**
+     * Main validator action to start validation process.
+     *
+     * @param array $data
+     *
+     * @return bool
+     */
+    public function validate($data)
+    {
+        $validationResult = true;
+        $this->errors = [];
+
+        foreach ($this->rules as $ruleClass => $ruleConfiguration) {
+            foreach ($ruleConfiguration['fields'] as $fieldName) {
+                try {
+                    /** @var AbstractRule $rule */
+                    $rule = new $ruleClass($ruleConfiguration);
+                    $rule->process($fieldName, $data);
+                } catch (RuleFailed $exception) {
+                    $this->errors[$fieldName][] = $exception->getMessage();
+                    $validationResult = false;
+                }
+            }
+        }
+
+        return $validationResult;
+    }
+
+    /**
+     * Returns validation errors list
+     *
+     * @return array
+     */
+    public function getErrors()
+    {
+       return $this->errors;
     }
 }
--- Tests/ValidatorTest.php
+++ Tests/ValidatorTest.php
@@ -2,13 +2,74 @@
 
 namespace Sieg\ArrayValidator\Tests;
 
+use Sieg\ArrayValidator\Rule\Required;
 use Sieg\ArrayValidator\Validator;
 
 class ValidatorTest extends \PHPUnit_Framework_TestCase
 {
+    public $configurationExample = [
+        Required::class => [
+            'fields' => ['field1', 'field2', 'field3']
+        ],
+//        Length::class => [
+//            [
+//                'fields' => ['field2', 'field3'],
+//                'message' => 'Field length should be more than 3',
+//                'moreThan' => 3
+//            ],
+//            [
+//                'fields' => ['field2', 'field3'],
+//                'message' => 'Field length should be less than 3',
+//                'lessThan' => 10
+//            ],
+//        ],
+//        Equals::class => [
+//            'fields' => ['password'],
+//            'message' => 'Some message',
+//            'equalToField' => 'password2'
+//        ]
+    ];
+
     public function testConstructor()
     {
-        $container = new Validator();
-        $this->assertInstanceOf(Validator::class, $container);
+        $validator = new Validator();
+        $this->assertInstanceOf(Validator::class, $validator);
+    }
+
+    public function testValidate()
+    {
+        $data = [
+            'field1' => 'value1',
+            'field2' => 'value2',
+            'field3' => 'value3'
+        ];
+
+        $validator = new Validator($this->configurationExample);
+        $this->assertTrue($validator->validate($data));
+    }
+
+    public function testEmptyGetErrors()
+    {
+        $validator = new Validator();
+        $this->assertEmpty($validator->getErrors());
+    }
+
+    public function testOneError()
+    {
+        $data = [
+            'field1' => 'value1',
+            'field2' => 'value2'
+        ];
+
+        $validator = new Validator($this->configurationExample);
+        $validator->validate($data);
+
+        $expected = [
+            'field3' => [
+                'Value required.'
+            ]
+        ];
+
+        $this->assertEquals($expected, $validator->getErrors());
     }
 }
